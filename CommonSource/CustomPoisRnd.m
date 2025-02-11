aae2313324f75e4fb6449324406ea62feedb2062
Han, Seungyeop <novelhan@gatech.edu>
2025-02-11 11:34:01 -0500

    Customized the random number generator

--- CommonSource/CustomPoisRnd.m
+++ CommonSource/CustomPoisRnd.m
@@ -10,6 +10,26 @@
 % Y: Generate Random Number
 
 function [Y] = CustomPoisRnd(p, m, n)
+    %.. To boost the computation speed, we precompute the factorial
+    persistent FACTORIAL
+    if isempty(FACTORIAL)
+        FACTORIAL = [  1
+                       1
+                       2
+                       6
+                      24
+                     120
+                     720
+                    5040
+                   40320
+                  362880
+                 3628800
+                39916800
+               479001600
+              6227020800
+             87178291200];
+    end
+
     % Input Handling
     if nargin == 1
         m = 1;
@@ -20,7 +40,8 @@ function [Y] = CustomPoisRnd(p, m, n)
     
     % Compute Poisson CDF (consider upto mean + 10-sigma)
     Zset = 0:1:ceil(11*p);
-    PDF = p.^Zset*exp(-p)./factorial(Zset); 
+%     PDF = p.^Zset*exp(-p)./factorial(Zset); 
+    PDF = p.^Zset*exp(-p)./FACTORIAL(Zset+1); 
     CDF = cumsum(PDF);
     CDF(end) = 1;
     
